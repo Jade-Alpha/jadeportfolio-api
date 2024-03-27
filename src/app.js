5f2191f2c49e3aac4d03dbf67404e9d813c4199a
apoerakay <apoerak@gmail.com>
2024-03-27 17:41:22 +0000

    swagger update

--- src/app.js
+++ src/app.js
@@ -2,10 +2,17 @@ import  express  from "express";
 import router from "./routes/allRoutes.js";
 import mongoose from "mongoose";
 import cors from "cors"
+import { handleRequests, init, handleResponses } from "express-oas-generator";
 import dotenv from "dotenv"
 
+
+const modelNames = mongoose.modelNames();
+
+const app = express(); 
+handleResponses(app, {});
+
 dotenv.config()
-const app = express ()
+
 
 const mongoURI = process.env.MONGO_URI
 
@@ -23,4 +30,33 @@ app.use(router);
 
 app.listen(PORT,() =>{
     console.log(`express app is running${PORT}`);
-});
+
+    init(
+        app,
+        (spec) => {
+          spec.info = {
+            title: "Portfolio API Documentation",
+            description: "API Documentation for Portfolio website",
+          };
+          spec.host = "jadeportfolio-api.onrender.com";
+          spec.schemes = ["http", "https"];
+    
+          return spec;
+        },
+        "/swagger.json",
+        60 * 1000,
+        "api-docs",
+        modelNames,
+        ["users"],
+        ["development"],
+        true
+      );
+    
+      console.log(
+        "Server Listening on  8080, Open http://localhost:8080/api-docs/"
+      );
+    });
+    
+    app.use(router);
+    
+    handleRequests();
